25d6fb11705fe8f75ae18f1926d1d77f2cfb940f
hyannu <annurao9560@gmail.com>
2026-07-01 22:34:47 +0530

    Initialize new game button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
-let newGameBtn = document.querySelector()
+let newGameBtn = document.querySelector("#new-btn");
 
 let turnO =true; //playerX,playerO
 
